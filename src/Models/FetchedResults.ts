dbc5ddcf8952e7fa178b7b6b802f575030e1f087
jfaponte403 <jfaponte403@gmail.com>
2025-11-15 22:08:43 -0500

    Refactor: Remove unused operations, refine `IFetchResponse`, and update TypeScript configs for stricter checks.

--- src/Models/FetchedResults.ts
+++ src/Models/FetchedResults.ts
@@ -1,14 +1,20 @@
+import {SupportedOperations} from "./SupportedOperations.ts";
+
 export interface IFetchResponse {
   result: number;
   message: string;
-  new: string;
+  num1: number;
+  num2: number;
+  operation: SupportedOperations;
 }
 
 export const defaultFetchedValues: IFetchResponse = {
   result: 0,
   message: '',
-  new: '',
-}
+  num1: 0,
+  num2: 0,
+  operation: SupportedOperations.ADDITION,
+};
 
 export type ButtonProps = {
   value: string;
--- src/Models/SupportedOperations.ts
+++ src/Models/SupportedOperations.ts
@@ -3,12 +3,4 @@ export enum SupportedOperations {
   DIVISION = 'division',
   ADDITION = 'sum',
   SUBTRACTION = 'subtract',
-  SQUARE_ROOT = 'sqrt',
-}
-
-export enum SupportedOperationsSymbols {
-  MULTIPLICATION = '×',
-  DIVISION = '÷',
-  ADDITION = '+',
-  SUBTRACTION = '-',
 }
